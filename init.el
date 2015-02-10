6c92ef8f8dac5346114c94ee763087b3310b2f28
cliff xuan <cliff.xuan@gmail.com>
2015-02-10 23:34:53 +0000

    melpa

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
 (require 'package)
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
 (when (not package-archive-contents)
