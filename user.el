b4f8fc465fe14cc79a036e5e20cefc4b82c1a9d8
cliff xuan <cliff.xuan@gmail.com>
2015-04-01 15:31:04 +0100

    2 space for js indent

--- user.el
+++ user.el
@@ -459,5 +459,10 @@ If the file is Emacs LISP, run the byte compiled version if exist."
 (when (fboundp 'winner-mode)
   (winner-mode 1))
 
+
+;; js indent
+(defvar js-indent-level)
+(setq js-indent-level 2)
+
 (provide 'user)
 ;;; user.el ends here
