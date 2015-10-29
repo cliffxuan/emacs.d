56bcc1bae501a56c429733cbf70a17812ea81f75
cliff xuan <cliff.xuan@gmail.com>
2015-10-29 13:53:04 +0000

    updated install script

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
-curl -fsSkL https://raw.github.com/cask/cask/master/go | python
+if [ ! -f "$HOME/bin/cask" ]
+then
+    curl -fsSkL https://raw.github.com/cask/cask/master/go | python
+fi
 if [ ! -d  "$HOME/bin" ]
 then
     mkdir $HOME/bin
 fi
-ln -s ~/.cask/bin/cask ~/bin
+ln -sf ~/.cask/bin/cask ~/bin
 if [ -d "$HOME/.emacs.d" ]
 then
     echo "rename existing .emacs.d to .emacs.d.old"
     mv $HOME/.emacs.d $HOME/.emacs.d.old
 fi
-ln -s $BASE_DIR $HOME/.emacs.d
+ln -sf $BASE_DIR $HOME/.emacs.d
